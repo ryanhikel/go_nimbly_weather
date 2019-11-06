4324258724c1ebb0f8876eb97181daa01e48d0a3
Ryan Hikel <hikelryan@gmail.com>
2019-11-06 05:51:56 -0600

    Added base weather fetching for one city

--- client/src/App.js
+++ client/src/App.js
@@ -3,45 +3,43 @@ import './App.css';
 
 class App extends Component {
   // Initialize state
-  state = { passwords: [] }
+  constructor(props){
+    super(props);
+    this.state = {
+      weather: []
+    }
+  }
 
   // Fetch passwords after first mount
   componentDidMount() {
-    this.getPasswords();
+    this.getWeather();
   }
 
-  getPasswords = () => {
+  getWeather(){
     // Get the passwords and store them in state
-    fetch('/api/passwords')
+    fetch('/weather')
       .then(res => res.json())
-      .then(passwords => this.setState({ passwords }));
+      .then(weather => this.setState({weather: weather.consolidated_weather}));
   }
 
   render() {
-    const { passwords } = this.state;
-
+    console.log(this.state.weather);
     return (
       <div className="App">
         {/* Render the passwords if we have them */}
-        {passwords.length ? (
+        {this.state.weather.length ? (
           <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-              {passwords.map((password, index) =>
+            <h1>London Weather</h1>
+            <ul className="forecast">
+              {this.state.weather.map((data, index) =>
                 <li key={index}>
-                  {password}
+                  {data.applicable_date}
                 </li>
               )}
             </ul>
             <button
               className="more"
-              onClick={this.getPasswords}>
+              onClick={this.getWeather}>
               Get More
             </button>
           </div>
@@ -51,7 +49,7 @@ class App extends Component {
               <h1>No passwords :(</h1>
               <button
                 className="more"
-                onClick={this.getPasswords}>
+                onClick={this.getWeather}>
                 Try Again?
             </button>
             </div>
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
-
+const request =  require('request');
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
+app.get('/weather', (req, res) => {
   const count = 5;
 
   // Generate some passwords
@@ -17,11 +17,20 @@ app.get('/api/passwords', (req, res) => {
   )
 
   // Return them as json
-  res.json(passwords);
+  // res.json(passwords);
 
   console.log(`Sent ${count} passwords`);
-});
 
+  let url = `https://www.metaweather.com/api/location/44418/`
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(JSON.parse(body));
+      console.log(JSON.parse(body));
+    }
+  });
+});
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
