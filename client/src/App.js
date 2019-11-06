d2fd294705a507ced37ea44480dce9e73788f351
Ryan Hikel <hikelryan@gmail.com>
2019-11-06 06:58:36 -0600

    added basic search and display of weather

--- client/src/App.js
+++ client/src/App.js
@@ -3,55 +3,60 @@ import './App.css';
 
 class App extends Component {
   // Initialize state
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       weather: []
     }
+    this.onFormChange = this.onFormChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
-  // Fetch passwords after first mount
-  componentDidMount() {
-    this.getWeather();
+  onFormChange(evt) {
+    const element = evt.target;
+    const name = element.name;
+    const value = element.value;
+    const newState = {};
+    newState[name] = value;
+    this.setState(newState);
   }
 
-  getWeather(){
-    // Get the passwords and store them in state
-    fetch('/weather')
+  onFormSubmit(evt) {
+    evt.preventDefault();
+    fetch(`/api/weather/${this.state.city}`)
       .then(res => res.json())
-      .then(weather => this.setState({weather: weather.consolidated_weather}));
+      .then(data => {
+        this.setState({ weather: data.consolidated_weather, location: data.title })
+      });
+
   }
 
   render() {
-    console.log(this.state.weather);
+    console.log(this.state);
     return (
-      <div className="App">
+      <div className="App container">
+        <fieldset>
+          <form onChange={this.onFormChange} onSubmit={this.onFormSubmit}>
+            <input name="city" type="text" className="ghost-input" placeholder="Enter a City" required />
+            <input type="submit" className="ghost-button" value="Get Weather" />
+          </form>
+        </fieldset>
         {/* Render the passwords if we have them */}
-        {this.state.weather.length ? (
+        {this.state.weather.length > 0 ? (
           <div>
-            <h1>London Weather</h1>
+            <h1>{this.state.location} Weather</h1>
             <ul className="forecast">
-              {this.state.weather.map((data, index) =>
+              {this.state.weather.map((day, index) =>
                 <li key={index}>
-                  {data.applicable_date}
+                  {day.applicable_date} - {day.weather_state_name}
                 </li>
               )}
             </ul>
-            <button
-              className="more"
-              onClick={this.getWeather}>
-              Get More
-            </button>
           </div>
         ) : (
             // Render a helpful message otherwise
             <div>
-              <h1>No passwords :(</h1>
-              <button
-                className="more"
-                onClick={this.getWeather}>
-                Try Again?
-            </button>
+              <h1>No data :(</h1>
             </div>
           )}
       </div>
--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 const express = require('express');
 const path = require('path');
-const generatePassword = require('password-generator');
+const bodyParser = require('body-parser');
 const request =  require('request');
 const app = express();
 
 // Serve static files from the React app
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-app.get('/weather', (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
-
-  // Return them as json
-  // res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
-
-  let url = `https://www.metaweather.com/api/location/44418/`
-  request(url, function (err, response, body) {
-    if (err) {
+app.get('/api/weather/:location', (req, res) => {
+  let city = req.params.location;
+  let locationSearch = `https://www.metaweather.com/api/location/search/?query=${city}`;
+  request(locationSearch, function (err, response, body) {
+    if (err && JSON.parse(body)[0]) {
       console.log(err);
     } else {
-      res.json(JSON.parse(body));
-      console.log(JSON.parse(body));
+      let woeid = JSON.parse(body)[0].woeid;
+      let url = `https://www.metaweather.com/api/location/${woeid}/`
+      console.log(url);
+      request(url, function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(JSON.parse(body));
+        }
+      });
     }
   });
+  
 });
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -40,4 +39,4 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`Password generator listening on ${port}`);
+console.log(`Weather App listening on ${port}`);
